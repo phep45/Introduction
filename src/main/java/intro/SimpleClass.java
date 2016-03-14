37cd9e8e1e28cd104216fb0f4d7566d747c4592e
Prosner <phep45@gmail.com>
2016-03-14 12:47:47 +0100

    init

---
+++ src/main/java/intro/SimpleClass.java
@@ -0,0 +1,12 @@
+package intro;
+
+/**
+ * Created by Prosner on 3/14/2016.
+ */
+public class SimpleClass {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
